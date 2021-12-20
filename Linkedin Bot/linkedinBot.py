bb747d5b8b18bb6a668ce016f014169f8ceb092f
Benjamin Cowen <bcowen@cardeabio.com>
2021-12-19 19:10:07 -0800

    Create linkedinBot.py

---
+++ Linkedin Bot/linkedinBot.py
@@ -0,0 +1,27 @@
+import requests
+import selenium
+from selenium import webdriver
+
+username = "mmonnneyy@gmail.com"
+password = "23Bccb72"
+
+
+driver = webdriver.Chrome()
+
+
+
+def get_session_cookies(username,password):
+	login_url = "https://www.linkedin.com/login"
+
+
+
+def get_user_job_history(username):
+	pass 
+
+
+
+
+def get_users_matching_criteria(past_jobs=[],current_job=[], education=[]):
+	pass
+
+	
