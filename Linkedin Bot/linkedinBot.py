c6055c5c02a964f92a3ca760f85939499ab47290
Benjamin Cowen <bcowen@cardeabio.com>
2021-12-19 19:39:09 -0800

    added login capabilities

--- Linkedin Bot/linkedinBot.py
+++ Linkedin Bot/linkedinBot.py
@@ -1,17 +1,34 @@
 import requests
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 username = "mmonnneyy@gmail.com"
-password = "23Bccb72"
+password = "23bccb72"
 
 
 driver = webdriver.Chrome()
 
 
 
+
+
+
+
+
 def get_session_cookies(username,password):
 	login_url = "https://www.linkedin.com/login"
+	driver.get(login_url)
+	actions = ActionChains(driver)
+	actions.send_keys(username)
+	actions.send_keys(Keys.TAB)
+	actions.send_keys(password)
+	actions.send_keys(Keys.TAB)
+	actions.send_keys(Keys.TAB)
+	actions.send_keys(Keys.TAB)
+	actions.send_keys(Keys.ENTER)
+	actions.perform()
 
 
 
@@ -24,4 +41,9 @@ def get_user_job_history(username):
 def get_users_matching_criteria(past_jobs=[],current_job=[], education=[]):
 	pass
 
-	
+
+
+
+
+
+cookies = get_session_cookies(username, password)
