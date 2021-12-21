5ce1772815c36eb30159f73a04e69e82cd76ca2a
Benjamin Cowen <bcowen@cardeabio.com>
2021-12-20 19:02:25 -0800

    added link feature
    linking feature enabled

--- Linkedin Bot/linkedinBot.py
+++ Linkedin Bot/linkedinBot.py
@@ -12,17 +12,29 @@ driver = webdriver.Chrome()
 
 
 
-
-
+def find_elements_by_inner_text(inner_text):
+	while (len(driver.find_elements_by_xpath(f'//*[text()[contains(., "{inner_text}")]]')) == 0):
+		pass
+	return driver.find_elements_by_xpath(f'//*[text()[contains(., "{inner_text}")]]')
 
 
 
 def get_session_cookies(username,password):
+	print("Logging in")
 	login_url = "https://www.linkedin.com/login"
 	driver.get(login_url)
 	actions = ActionChains(driver)
 	actions.send_keys(username).send_keys(Keys.TAB).send_keys(password).send_keys(Keys.TAB).send_keys(Keys.TAB).send_keys(Keys.TAB).send_keys(Keys.ENTER)
 	actions.perform()
+	cookies = {}
+	selenium_cookies = driver.get_cookies()
+	selenium_cookies_s = selenium_cookies
+	for cookie in selenium_cookies:
+		cookies[cookie['name']] = cookie['value']
+	return cookies
+	print(username, "logged in")
+
+
 
 
 
@@ -32,12 +44,36 @@ def get_user_job_history(username):
 
 
 
-def get_users_matching_criteria(past_jobs=[],current_job=[], education=[]):
-	pass
+def get_users_matching_criteria_url(past_job=[],current_job=[], education=[]):
+	print("Determining search critera url")
+	driver.get("https://www.linkedin.com/search/results/people/?origin=SWITCH_SEARCH_VERTICAL&sid=9~1")
 
-
-
+	find_elements_by_inner_text("All filters")[0].click()
+	for edu in education:
+		print("adding education", edu)
+		find_elements_by_inner_text("Add a school")[0].click()
+		actions = ActionChains(driver)
+		actions.send_keys(edu).pause(3).send_keys(Keys.DOWN).send_keys(Keys.ENTER).pause(3)
+		actions.perform()
+	for job in past_job:
+		print("adding past job ", job)
+		find_elements_by_inner_text("Add a company")[1].click()
+		actions = ActionChains(driver)
+		actions.send_keys(job).pause(3).send_keys(Keys.DOWN).send_keys(Keys.ENTER).pause(3)
+		actions.perform()
+	for job in current_job:
+		print("adding current job ", job)
+		find_elements_by_inner_text("Add a company")[0].click()
+		actions = ActionChains(driver)
+		actions.send_keys(job).pause(3).send_keys(Keys.DOWN).send_keys(Keys.ENTER).pause(3)
+		actions.perform()
+	find_elements_by_inner_text("Show results")[0].click()
+	return driver.current_url
 
 
 
 cookies = get_session_cookies(username, password)
+url = get_users_matching_criteria_url(education=["UC Berkeley", "Stanford"], past_job=["Cisco","Palo Alto Networks"])
+
+
+
